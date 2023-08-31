11c5c2ffd8c5207408c8aa8090bc37e915b13ccf
ismail <yako_is@yahoo.fr>
2023-08-31 23:17:07 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let i = 0,
+    j = word.length - 1
+  while (i <= j) {
+    if (word[i] == word[j]) {
+      i++
+      j--
+    } else {
+      return false
+    }
+    return true
+  }
 }
 
+
+// Big O : time complexity : n(o)
 /* 
   Add your pseudocode here
 */
 
 /*
   Add written explanation of your solution here
+  two pointers that moves to the middle(from the start and the end of the word) 
+  if letters don't match we return false otherwise continue moving the pointers 
+  return true 
 */
 
 // You can run `node index.js` to view these console logs
